c431908823815632d44d2a8374bb2bed1a291f5a
Matti Pastell <matti.pastell@helsinki.fi>
2022-01-18 10:06:56 +0200

    Add example on reading and inspecting APSIM simulation

---
+++ TaskFileConverter/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using APSIM.Shared.Utilities;
+using Models.Factorial;
+using Models.Core;
+using Models.Core.ApsimFile;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+
+string apsimxFilePath = @"..\..\..\..\Examples\Wheat.apsimx";
+Simulations file = FileFormat.ReadFromFile<Simulations>(apsimxFilePath, e => throw e, false);
+
+// Loop trough toplevel children
+//file.Children.ForEach(child => Console.WriteLine(child.Name));
+
+// Test selecting components https://apsimnextgeneration.netlify.app/usage/pathspecification/
+//IVariable soil1 = file.FindByPath("[Soil]");
+IVariable isoil = file.FindByPath("[Field].Soil");
+var soil = (Models.Soils.Soil)isoil.Value;
+Console.WriteLine("Soil children:");
+foreach (var child in soil.Children){
+    Console.WriteLine("\t" + child.Name);
+}
+
+Console.WriteLine("Done!");
