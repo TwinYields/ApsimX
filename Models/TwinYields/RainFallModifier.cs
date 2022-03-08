95c535595bb09e1618696e21922a78b23f97311a
Matti Pastell <matti.pastell@helsinki.fi>
2022-03-08 10:49:01 +0200

    Add example model for modifying rainfall and stuble for ZMQ communication

---
+++ Models/TwinYields/RainFallModifier.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Models.Core;
+using Models.Climate;
+using Models.Interfaces;
+using Models.PMF;
+using APSIM.Shared.Utilities;
+
+namespace Models
+{
+    /// <summary>This is a simple rainfall modifier model.</summary>
+    [Serializable]
+    [ValidParent(ParentType = typeof(Simulation))]
+    [ViewName("UserInterface.Views.PropertyView")]
+    [PresenterName("UserInterface.Presenters.PropertyPresenter")]
+
+    public class RainfallModifier : Model
+    {
+        [Link]
+        Clock clock = null;
+
+        [Link]
+        Weather weather = null;
+
+        /// <summary>Start date for modifying rainfall</summary>
+        [Description("Start modifying rainfall from date:")]
+        public DateTime StartDate { get; set; }
+
+        /// <summary>Rainfall muliplier</summary>
+        [Description("Rainfall multiplier: ")]
+        public double RainfallMultiplier { get; set; }
+
+        /// <summary>Rainfall addition</summary>
+        [Description("Rainfall addition (mm): ")]
+        public double RainfallAddition { get; set; }
+
+        /// <summary>An output variable.</summary>
+        public double OriginalRain { get; private set; }
+
+        /// <summary>Handler for event invoked by weather model to allow modification of weather variables.</summary>
+        [EventSubscribe("PreparingNewWeatherData")]
+        private void ModifyWeatherData(object sender, EventArgs e)
+        {
+            OriginalRain = weather.Rain;
+            Console.WriteLine(OriginalRain.ToString());
+            if (clock.Today >= StartDate)
+                weather.Rain = RainfallMultiplier * weather.Rain + RainfallAddition;
+        }
+    }
+}
---
+++ Models/TwinYields/SendReceive.cs
@@ -0,0 +1,49 @@
+﻿namespace Models
+{
+    using Models.Core;
+    using Models.Climate;
+    using Models.Interfaces;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Models.Soils;
+    using Models.PMF;
+    using APSIM.Shared.Utilities;
+  
+
+    /// <summary>
+    /// Monitor and influence simulation using ZMQ sockets
+    /// </summary>
+    [Serializable]
+    [ViewName("UserInterface.Views.PropertyView")]
+    [PresenterName("UserInterface.Presenters.PropertyPresenter")]
+    [ValidParent(ParentType = typeof(Simulation))]
+    public class SendReceive : Model
+    {
+        [Link]
+        Clock clock = null;
+
+        //[Link]
+        //Weather weather = null;
+
+        //[Link]
+        //Soil soil = null;
+
+        [Link]
+        private Simulation simulation = null;
+
+        /// <summary>HostName</summary>
+        [Description("ZeroMQ host url: ")]
+        public String HostURL { get; set; }
+        //private String HostURL = "";
+
+        [EventSubscribe("DoManagement")]
+        private void DoDailyCalculations(object sender, EventArgs e)
+        {
+            Console.WriteLine(clock.ToString());
+            var sim2 = simulation;
+            Console.Write(HostURL);
+        }
+
+    }
+}
